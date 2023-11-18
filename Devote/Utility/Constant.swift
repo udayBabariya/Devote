990df310f1097ad1f0cb58e7c90970df1f05c46d
Uday Babariya <uday.babariya.77@gmail.com>
2023-11-18 08:39:28 +0530

    basic dore data reformatting

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Uday on 18/11/23.
+//
+
+import SwiftUI
+
+//MARK: - Formater
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+//MARK: - UI
+
+//MARK: - UX
