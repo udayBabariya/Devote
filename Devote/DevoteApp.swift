356815cd93c547f3fee3f27560e49fa44bb80cc0
Uday Babariya <uday.babariya.77@gmail.com>
2023-11-18 08:09:34 +0530

    Initial Commit

---
+++ Devote/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Uday on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
